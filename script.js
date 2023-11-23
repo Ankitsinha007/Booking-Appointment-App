0840abe0b6498f102d498634882f30da972885a5
Ankitsinha007 <sinhaankit0710@gmail.com>
2023-11-23 12:31:31 +0530

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 
-const apiUrl = 'https://crudcrud.com/api/c96d8b026eba467691c86b5cc4208e36/bookings'; // Replace with your server API URL
+const apiUrl = 'https://crudcrud.com/api/9f4a04bbdec44657be9659a026890bdd/bookings'; // Replace with your server API URL
 const bookingForm = document.getElementById('bookingForm');
 const bookingList = document.getElementById('bookingList');
 
@@ -20,7 +20,7 @@ async function submitBooking() {
         // Fetch and display updated bookings
         fetchAndDisplayBookings();
     } catch (error) {
-        console.error('Error submitting booking:', error);
+        console.log( error);
     }
 }
 
@@ -55,31 +55,60 @@ async function fetchAndDisplayBookings() {
         const response = await axios.get(apiUrl);
         displayBookings(response.data);
     } catch (error) {
-        console.error('Error fetching bookings:', error);
+        console.log( error);
     }
 }
 
-// async function deleteBooking(id){
+async function deleteBooking(id){
+    try {
+            await axios.delete(`${apiUrl}/${id}`)
+
+            fetchAndDisplayBookings();
+    }
+    catch (err){
+        console.log(err);
+
+    }
+}
+
+// function deleteBooking(id){
+//     let getdetails = axios.get(apiUrl);
+//     console.log(getdetails)
+
+//     axios.delete(`${apiUrl}/${id}`)
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err))
+
+// }
+
+// async function deleteBooking(id) {
 //     try {
-//             await axios.delete(`${apiUrl}/${id}`)
+//         // Send a delete request to the server
+//         await axios.delete(`${apiUrl}/${id}`);
 
-//             fetchAndDisplayBookings();
-//     }
-//     catch (err){
-//         console.log(err);
-
+//         // Fetch and display updated appointments
+//         const response = await axios.get(apiUrl);
+//         // displayBookings(response.data);
+//         fetchAndDisplayBookings();
+//     } catch (error) {
+//         console.log('Error deleting appointment:', error);
 //     }
 // }
 
-function deleteBooking(id){
-    // let confirmation = window.confirm("Are you sure?");
+function editBooking(id, newData) {
+    // Assume you have a server endpoint for updating bookings
+    // Use axios.put to send updated data to the server
 
-    axios
-    .delete(`https://crudcrud.com/api/c96d8b026eba467691c86b5cc4208e36/bookings/${id}`)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-
-    fetchAndDisplayBookings();
+    axios.put(`/api/bookings/${id}`, newData)
+        .then(response => {
+            // Handle success
+            console.log('Booking updated successfully');
+            // You can update the UI or do other things after successful update
+        })
+        .catch(error => {
+            // Handle error
+            console.error('Error updating booking', error);
+        });
 }
 
 // Fetch and display initial bookings on page load
